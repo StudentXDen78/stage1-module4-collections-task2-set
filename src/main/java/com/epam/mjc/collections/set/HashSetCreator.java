eb565d4984f486cec0d71722c731a81f8f4cdf37
xden78 <venera.opt@gmail.com>
2024-11-07 22:52:56 +0300

    1

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -1,9 +1,29 @@
 package com.epam.mjc.collections.set;
 
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 public class HashSetCreator {
-    public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+    public static HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet<Integer> newHashSet = new HashSet<>();
+        for (Integer e : sourceList) {
+            if (e % 2 == 0) {
+                int element = e;
+                while (element % 2 == 0) {
+                    newHashSet.add(element);
+                    element = element / 2;
+                }
+            }
+            else {
+                newHashSet.add(e);
+                newHashSet.add(e * 2);
+            }
+        }
+        return newHashSet;
+        // [2, 1, -1, -2, 3, 6, 8, 4, -5, -10, 12]
+    }
+
+    public static void main(String[] args) {
+        Integer[] list = {2, -1, 3, 8, -5, 12};
+        createHashSet(new ArrayList<>(List.of(list)));
     }
 }
--- src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
@@ -1,9 +1,11 @@
 package com.epam.mjc.collections.set;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class SubsetOfSquaresCreator {
     public Set<Integer> createSubsetOfSquares(List<Integer> sourceList, int lowerBound, int upperBound) {
+        return new HashSet<>();
     }
 }
