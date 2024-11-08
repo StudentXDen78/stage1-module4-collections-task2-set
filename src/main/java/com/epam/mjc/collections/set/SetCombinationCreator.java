386b21f38f4056322e1250172999cab53ddd2d1a
xden78 <venera.opt@gmail.com>
2024-11-08 21:52:57 +0300

    4

--- src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
@@ -1,14 +1,28 @@
 package com.epam.mjc.collections.set;
 
-import java.util.HashSet;
-import java.util.NavigableSet;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class SetCombinationCreator {
-    public Set<String> createSetCombination(Set<String> firstSet, Set<String> secondSet, Set<String> thirdSet) {
-        // NavigableSet navigableSet = new TreeSet();
+    public static Set<String> createSetCombination(Set<String> firstSet, Set<String> secondSet, Set<String> thirdSet) {
+        Set<String> setCombination= new LinkedHashSet<>();
+        for (String element : firstSet) {
+            if (secondSet.contains(element) && !thirdSet.contains(element)) {
+               setCombination.add(element);
+            }
+        }
 
-        return new HashSet<>();
+        for (String element : thirdSet) {
+            if (!firstSet.contains(element) && !secondSet.contains(element)) {
+                setCombination.add(element);
+            }
+        }
+        return setCombination;
+    }
+
+    public  static  void main(String[] args) {
+        Set<String> f = new LinkedHashSet<>(Arrays.asList(new String[]{"Java", "Collection",  "framework", "interface", "class", "Queue"}));
+        Set<String> s = new LinkedHashSet<>(Arrays.asList(new String[]{"Queue", "iterator", "Java", "Collection","comparator"}));
+        Set<String> th = new LinkedHashSet<>(Arrays.asList(new String[]{"Java", "Set", "Map", "List"}));
+        createSetCombination(f, s, th);
     }
 }
